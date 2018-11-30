33c6459f83bf5213593b6b69cb21f2c8fe4149eb
Jaehyuk Rhee <jaehyuk.rhee@gmail.com>
2018-11-30 11:17:00 -0500

    clean up for repo

--- build/routes/lib/api/data.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var data_controllers_1 = require("../../../controllers/api/data-controllers");
-var router = express_1.Router();
-router.get("/search", data_controllers_1.searchAndCreate);
-exports.default = router;
-//# sourceMappingURL=data.js.map
--- build/routes/lib/web/restaurants.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var restaurants_1 = require("../../../controllers/web/restaurants");
-var passport_1 = require("../../../config/passport");
-var router = express_1.Router();
-router.get("/", passport_1.isAuthenticated, restaurants_1.getAll);
-exports.default = router;
-//# sourceMappingURL=restaurants.js.map
