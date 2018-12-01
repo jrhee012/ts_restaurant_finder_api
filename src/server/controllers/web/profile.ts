21880ce5545188e023d0616594746359148fb7eb
Jaehyuk Rhee <jaehyuk.rhee@gmail.com>
2018-12-01 18:55:25 -0500

    update user authentication flow

--- src/server/controllers/web/profile.ts
+++ src/server/controllers/web/profile.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import { isEmpty } from "lodash";
+import { UsersModel } from "../../models/lib/Users";
+import { Users } from "../../models";
 
 export const getProfile = async (req: Request, res: Response) => {
     const data = { user: res.locals.user };
@@ -17,4 +19,28 @@ export const saveProfile = async (req: Request, res: Response) => {
 export const getAuthenticationPage = (req: Request, res: Response) => {
     const data = { user: res.locals.user };
     return res.status(200).render("pages/profile/authenticate_form");
+};
+
+export const postAuthenticationPage = async (req: Request, res: Response) => {
+    const params = req.body;
+    if (params.email === undefined || params.email === null) {
+        return res.redirect("/profile/authenticate");
+    }
+    if (params.password === undefined || params.password === null) {
+        return res.redirect("/profile/authenticate");
+    }
+
+    const user = res.locals.user;
+    try {
+        user.local.email = params.email;
+        user.setPassword(params.password);
+        user.authenticated = true;
+        await user.save();
+    } catch (e) {
+        console.log("ERROR post authenticate user controller");
+        console.error(e.message);
+        return res.redirect("/");
+    }
+
+    return res.redirect("/profile");
 };
--- src/server/routes/lib/web/profile.ts
+++ src/server/routes/lib/web/profile.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { getProfile } from "../../../controllers/web/profile";
+import { getProfile, getAuthenticationPage, postAuthenticationPage } from "../../../controllers/web/profile";
 
 const router: Router = Router();
 
 router.get("/", getProfile);
+
+router.get("/authenticate", getAuthenticationPage);
+router.post("/authenticate", postAuthenticationPage);
 
 export default router;
