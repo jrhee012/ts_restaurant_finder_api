02b060fa9f8f4f1d32c56bb33cc21df8337ef147
Jaehyuk Rhee <jaehyuk.rhee@gmail.com>
2018-12-02 04:52:44 -0500

    profile page update heroku build

---
+++ build/controllers/admin/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.homePage = function (req, res) {
+    return res.status(200).render("pages/admin/index");
+};
+//# sourceMappingURL=index.js.map
