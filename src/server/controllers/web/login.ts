3d5d06572b79b188e93e72986cfe0118154154e9
Jaehyuk Rhee <jaehyuk.rhee@gmail.com>
2018-12-01 19:25:09 -0500

    fix alerts

--- src/server/controllers/web/login.ts
+++ src/server/controllers/web/login.ts
@@ -2,17 +2,15 @@ import { Request, Response } from "express";
 
 export const getLogin = (req: Request, res: Response) => {
     console.log("login page!");
-    console.log(req.flash("error"));
-    const data = {
-        user: res.locals.user,
-        alert: req.flash("error"),
-    };
+    // console.log(req.flash("error"));
+    const flash = req.flash("error");
+    const data = { user: res.locals.user, alert: flash };
     return res.status(200).render("pages/login/login", data);
 };
 
 export const getSignUp = (req: Request, res: Response) => {
     console.log("sign up page!");
-    console.log(req.flash("error"));
-    const data = { user: res.locals.user };
+    const flash = req.flash("error");
+    const data = { user: res.locals.user, alert: flash };
     return res.status(200).render("pages/login/signup", data);
 };
