ad2bd65155dd41db6bd7fe7d0a8dfe4176c43683
Jaehyuk Rhee <jaehyuk.rhee@gmail.com>
2018-12-02 02:57:08 -0500

    clean up for repo

--- build/routes/lib/admin/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var admin_1 = require("../../../controllers/admin");
-var router = express_1.Router();
-router.get("/", admin_1.homePage);
-exports.default = router;
-//# sourceMappingURL=index.js.map
