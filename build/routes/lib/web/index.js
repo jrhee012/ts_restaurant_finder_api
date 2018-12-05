ae2e0eca149b06d1b55aba0b97be5f06ccd521f3
Jaehyuk Rhee <jaehyuk.rhee@gmail.com>
2018-12-05 11:35:12 -0500

    update build

--- build/routes/lib/web/index.js
+++ build/routes/lib/web/index.js
@@ -8,9 +8,10 @@ var passport_1 = require("../../../config/passport");
 var profile_1 = __importDefault(require("./profile"));
 var restaurants_1 = __importDefault(require("./restaurants"));
 var tickets_1 = __importDefault(require("./tickets"));
+var utils_1 = require("../../../utils");
 var router = express_1.Router();
 router.use("/profile", passport_1.isAuthenticated, profile_1.default);
-router.use("/restaurants", passport_1.isAuthenticated, restaurants_1.default);
+router.use("/restaurants", passport_1.isAuthenticated, utils_1.checkAuthenticated, restaurants_1.default);
 router.use("/tickets", passport_1.isAuthenticated, tickets_1.default);
 exports.default = router;
 //# sourceMappingURL=index.js.map
